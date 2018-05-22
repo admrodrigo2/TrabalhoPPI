14c5c91efe21459ef7c4a9c533e22c32fe1b4991
admrodrigo2 <admrodrigo2@yahoo.com.br>
2018-05-21 23:36:24 -0300

    Modificações

--- app/modules/contato/contato.php
+++ app/modules/contato/contato.php
@@ -1,3 +1,76 @@
 <div class="tab" id="contato">
-	<h3>Página Contato</h3>
+
+	<section class="section section-top section-full">
+
+	  <div class="bg-cover" style="background-image: url(assets/images/bg-contato.jpeg);"></div>
+	  <div class="bg-overlay"></div>
+	  <div class="container">
+	        <div class="row justify-content-center align-items-center">
+	          <div class="col-md-8 col-lg-7">
+
+	            <!-- Preheading -->
+	            <p class="text-white text-muted text-uppercase text-center text-sm">
+	              Clinica Rodrigão
+	            </p>
+	            
+	            <!-- Heading -->
+	            <h1 class="text-white text-center mb-4">
+	              Contato
+	            </h1>
+
+	            <!-- Subheading -->
+	            <p class="lead text-white text-muted text-center mb-5 animate" data-animation="fadeUp" data-animation-order="2" data-animation-trigger="entered">
+	              Entre em contato! Nos envie comentários, sugestões ou reclamações. Estamos sempre prontos para melhor lhe atender.;
+	            </p>
+
+	            <!-- Button -->
+	            <p class="text-center text-muted mb-0">
+	              Faça parte dessa família!
+	            </p>
+
+	          </div>
+	        </div> <!-- / .row -->
+	      </div>
+	  
+	</section>
+
+	<div class="container section" id="main">
+		<div class="row align-items-center">
+			
+			<div class="col-md-6 order-md-1 text-left text-md-left pr-md-5">
+				
+				<h2>Deixe sua mensagem</h2><br>
+
+				<form action="/action_page.php">
+
+					<div class="form-group">
+						<label for="nome">Nome:</label>
+      					<input type="text" class="form-control" id="nome" name="nome" required>
+    				</div>
+
+    				<div class="form-group">
+ 						<label for="email">Email:</label>
+ 						<input type="email" class="form-control" id="email" name="email" required>
+ 					</div>
+
+ 					<div class="form-group">
+  						<label for="mensagem">Mensagem:</label>
+  						<textarea class="form-control" rows="5" id="mensagem"></textarea>
+					</div>
+
+					<div class="form-group"> 
+      					<button type="submit" class="btn btn-default">Enviar</button>
+  					</div>
+
+  				</form>
+			</div>
+
+			<div class="col-6 mx-auto col-md-6 order-md-2">
+				<img class="img-fluid img-responsive mb-3 mb-md-0" src="http://maps.googleapis.com/maps/api/staticmap?center=-18.9185061,-48.2581695
+&zoom=15&markers=color:red|-18.9185061,-48.2581695	&size=500x500">
+
+			</div>
+
+		</div>
+	</div>
 </div>
